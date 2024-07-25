7c79bd1b46584c0ff05dea4d5acaf5dd147384c0
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-26 06:32:34 +0900

    feat:Main_page 초기 화면 구현 스크롤 이동 구현 서비스 소개 및 게임 소개 네비게이션 바, 플레이 버튼

--- src/components/MainHomeFrame.jsx
+++ src/components/MainHomeFrame.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function MainHomeFrame(){
+const MainHomeFrame = ({ children }) => {
   return (
-    <div className="w-[1024px] h-[850px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] rounded-[30px] border-[10px] border-[rgba(255,255,255,0.2)]">
-      dlkjflksjdlkf
+    <div className="w-[1024px] h-[600px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] rounded-[30px] border-[10px] border-[rgba(255,255,255,0.2)] absolute z-10 flex justify-center items-center">
+      {children}
     </div>
-  )
-}
+  );
+};
+export default MainHomeFrame;
