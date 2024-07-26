20f9cf6ccc181de0fa188841f57e6bb0c40ff827
dhkang <dhkang960912@gmail.com>
2024-07-26 17:54:31 +0900

    feat:공통컴포넌트 제작 - 게임 순서 - SingleVideoView

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Play from "./pages/Play";
 import MyPage from "./pages/MyPage";
+import SingleVideoView from "./components/SingleVideoView";
+import GameTurns from "./components/GameTurns";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/mypage" element={<MyPage />} />
     </Routes>
+    <SingleVideoView data={{name:"김민영", mic:true, ready:true}}></SingleVideoView>
+    <GameTurns sectionNumber={3}></GameTurns>
     </>
   );
 }
---
+++ src/pages/SelfIntroduction.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const SelfIntroduction = ()=>{
+  return 
+}
+
+export default SelfIntroduction
