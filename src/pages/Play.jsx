77702e0c4f8a455f91e3f2558fa52567b21d5e13
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-28 19:53:05 +0900

    feat:배경, 네비게이션 바(Play_page)

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
+import React from "react";
+import NavBar from "../components/NavBar";
+import MainGradientBackground from "../components/MainGradientBackground";
+import MainHomeFrame from "../components/MainHomeFrame";
 
 const Play = () => {
   return (
     <>
-    <NavBar />
-    <div>
-      <h1>플레이 페이지</h1>
-      <p>방만들기, 입장하기</p>
-    </div>
+      <MainGradientBackground>
+        <MainHomeFrame>
+          <div className="flex flex-col items-center">
+            <div className="w-[800px]">
+              <NavBar />
+            </div>
+            <div className="w-[400px] h-[500px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] rounded-[30px] border-[10px] border-[rgba(255,255,255,0.2)] flex flex-col justify-between items-center p-5"></div>
+          </div>
+        </MainHomeFrame>
+      </MainGradientBackground>
     </>
-  )
-}
+  );
+};
 
 export default Play;
