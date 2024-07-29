a16c192fe01e7577712492825d1d9a8982ebb813
이명욱 <muk525@icloud.com>
2024-07-29 15:53:50 +0900

    feat:step 버튼 추가(Report)

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -27,14 +27,20 @@ const Report = () => {
               <div className="bg-white rounded-full h-[125px] w-[125px]"></div>
             </div>
 
-            <div className="flex gap-5">
-              <div className="w-[400px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] flex p-5 gap-5 relative">
+            <div className="flex gap-5 mt-5">
+              <div className="w-[400px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] rounded-[20px] flex p-5 gap-5 relative">
+                <div className="absolute top-[-10px] left-[-10px] rounded-[20px] bg-white w-[20%] h-[20%] flex justify-center items-center" >
+                  <h1>step 1</h1>
+                </div>
                 <Carousel>
                   <h1>나는 캐러셀이다.</h1>
                 </Carousel>
               </div>
 
-              <div className="w-[400px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] flex p-5 gap-5 relative">
+              <div className="w-[400px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] rounded-[20px] flex p-5 gap-5 relative">
+              <div className="absolute top-[-10px] left-[-10px] rounded-[20px] bg-white w-[20%] h-[20%] flex justify-center items-center" >
+                  <h1>step 2</h1>
+                </div>
                 <Carousel>
                   <h1>나도 캐러셀이다.</h1>
                 </Carousel>
@@ -42,7 +48,10 @@ const Report = () => {
             </div>
 
             <div>
-              <div className="w-[820px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] flex p-5 gap-5 relative">
+              <div className="w-[820px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] rounded-[20px] flex p-5 gap-5 relative">
+              <div className="absolute top-[-10px] left-[-10px] rounded-[20px] bg-white w-[10%] h-[20%] flex justify-center items-center">
+                  <h1>step 3</h1>
+                </div>
                 <Carousel>
                   <h1>애도 캐러셀이다.</h1>
                 </Carousel>
