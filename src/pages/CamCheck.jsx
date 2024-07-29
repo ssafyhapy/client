1a8993c7325a1238df994a580f9165f0f56727be
dhkang <dhkang960912@gmail.com>
2024-07-29 12:12:44 +0900

    feat:Cam_Check Page UI 구현 완료

--- src/pages/CamCheck.jsx
+++ src/pages/CamCheck.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import GameBackground from "../components/GameBackgound";
 import GameDescriptionFrame from "./../components/GameDescriptionFrame";
-import SingleVideoView from "../components/SingleVideoView";
-import { useState } from "react";
+import CameraCheckVideoView from "./../components/CameraCheckVideoView"
+import MicBtn from "./../components/btn/MicBtn"
+import MaskBtn from "./../components/btn/MaskBtn"
+
 
 const CamCheck = () => {
   const text = "나만의 가면을 선택하고 마이크와 비디오를 테스트해보세요!";
@@ -12,12 +14,12 @@ const CamCheck = () => {
       <div className="flex flex-col justify-center items-center p-[2rem] w-full">
         <div className="w-[70%]">
           <div className="bg-white flex flex-col justify-center items-center p-[2rem] rounded-[40px] shadow-[0_0_30px_rgba(66,72,81,0.2)] mb-[20px]">
-            <div className="w-[590px] h-[375px]">
-              <SingleVideoView
+            <div className="h-[28rem] flex justify-center items-center">
+              <CameraCheckVideoView
                 data={{ name: "someone", mic: false, ready: false }}
               />
             </div>
-            <div>마이크, 가면 선택</div>
+            <div className="flex justify-between"><span className="mr-10"><MicBtn/></span> <span><MaskBtn/></span></div>
           </div>
           <GameDescriptionFrame text={text} btnText={btnText} />
         </div>
