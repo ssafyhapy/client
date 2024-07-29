b27795b906a25da1b7859f9de1ad7bcea0f1b8ca
dhkang <dhkang960912@gmail.com>
2024-07-29 16:10:11 +0900

    feat:Cam_Check 나가기 버튼 추가

--- src/components/btn/ExitBtn.jsx
+++ src/components/btn/ExitBtn.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const ExitBtn = () => {
   return (
-    <button className="w-[65px] h-[30px] rounded-[30px] text-[#ffffff] text-[16px] bg-[rgba(255,86,117)] shadow-[0_4px_10px_rgba(66,72,81,0.5)]">
+    <button className="w-[65px] h-[30px] rounded-[30px] text-[#ffffff] text-[16px] bg-[rgba(255,86,117)] shadow-[0_4px_10px_rgba(66,72,81,0.5)] absolute right-10 top-5 z-20">
       나가기
     </button>
   );
-}
+};
 
-export default ExitBtn
+export default ExitBtn;
