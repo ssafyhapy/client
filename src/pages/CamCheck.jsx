af3c942444585dab61579b6e8a5c04f7466aacf0
dhkang <dhkang960912@gmail.com>
2024-07-29 13:40:58 +0900

    feat:Cam_Check Page 가면 미선택 문구 추가

--- src/pages/CamCheck.jsx
+++ src/pages/CamCheck.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import GameBackground from "../components/GameBackgound";
-import GameDescriptionFrame from "./../components/GameDescriptionFrame";
-import CameraCheckVideoView from "./../components/CameraCheckVideoView"
-import MicBtn from "./../components/btn/MicBtn"
-import MaskBtn from "./../components/btn/MaskBtn"
-
+import BasicBtn from "./../components/btn/BasicBtn";
+import CameraCheckVideoView from "./../components/CameraCheckVideoView";
+import MicBtn from "./../components/btn/MicBtn";
+import MaskBtn from "./../components/btn/MaskBtn";
 
 const CamCheck = () => {
   const text = "나만의 가면을 선택하고 마이크와 비디오를 테스트해보세요!";
   const btnText = "완료";
+  const mask = false
   return (
     <GameBackground>
       <div className="flex flex-col justify-center items-center p-[2rem] w-full">
@@ -19,9 +19,24 @@ const CamCheck = () => {
                 data={{ name: "someone", mic: false, ready: false }}
               />
             </div>
-            <div className="flex justify-between"><span className="mr-10"><MicBtn/></span> <span><MaskBtn/></span></div>
+            <div className="flex justify-between">
+              <span className="mr-20">
+                <MicBtn />
+              </span>
+              <span>
+                <MaskBtn />
+              </span>
+            </div>
           </div>
-          <GameDescriptionFrame text={text} btnText={btnText} />
+          <div className="w-[100%] h-[10rem] rounded-[40px] bg-[rgba(255,255,255,0.7)] shadow-[0_0_30px_rgba(66,72,81,0.2)] text-[#55B5EC] text-[24px] flex items-center justify-between p-[1rem] relative">
+            <div className="flex-grow flex items-center justify-center">
+              {text}
+            </div>
+            <div className={`text-[#FF607F] text-[12px] absolute bottom-12 right-4 ${mask===false ? null : "hidden"}`}>*가면을 선택해주세요</div>
+            <div className="absolute bottom-4 right-4">
+              <BasicBtn btnText={btnText} />
+            </div>
+          </div>
         </div>
       </div>
     </GameBackground>
