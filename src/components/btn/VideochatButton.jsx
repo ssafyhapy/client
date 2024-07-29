a9c49fafe051d38afcc516fff7f457e2492c1768
heejoo <heejookim99@gmail.com>
2024-07-29 09:55:11 +0900

    feat: VideochatButton 삭제 후 이모티콘, 마스크, 마이크 버튼으로 각각 생성 (components)

--- src/components/btn/VideochatButton.jsx
+++ src/components/btn/VideochatButton.jsx
@@ -10,8 +10,8 @@ const VideochatButton = () => {
           src="src/assets/smileyface.webp"
           alt="smileyface"
         />
-        {/* maskbutton */}
       </button>
+        {/* maskbutton */}
       <button className="relative z-10 w-[53px] h-[47px] shadow-[0_4px_10px_rgba(66,72,81,0.5)] rounded-2xl bg-[rgba(0,0,0,0.1)] flex items-center justify-center hover:bg-[rgba(0,0,0,0.5)]">
         <img
           className="w-[43px] h-[43px]"
