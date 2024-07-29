d55c8a78d96d979485d294c46e781fe395555955
이명욱 <muk525@icloud.com>
2024-07-29 10:52:04 +0900

    feat:배경, 프레임 구현(My_page)

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
+import React from "react";
+import NavBar from "../components/NavBar";
+import MainGradientBackground from "../components/MainGradientBackground";
+import MyPageFrame from "../components/MyPageFrame";
 
 const MyPage = () => {
   return (
     <>
-    <NavBar />
-    <div>
-      <h1>마이페이지</h1>
-      <p>회원정보</p>
-    </div>
+      <MainGradientBackground>
+        <MyPageFrame>
+          <div className="flex flex-col items-center gap-10">
+            <div className="w-[800px]">
+              <NavBar />
+            </div>
+            <div className="flex gap-10">
+              <div className="w-[400px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)]"></div>
+              <div className="w-[400px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)]"></div>
+            </div>
+            <div className="flex gap-10">
+              <div className="w-[400px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)]"></div>
+              <div className="w-[400px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)]"></div>
+            </div>
+            <div className="flex gap-10">
+              <div className="w-[840px] h-[200px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)]"></div>
+            </div>
+          </div>
+        </MyPageFrame>
+      </MainGradientBackground>
     </>
-  )
-}
+  );
+};
 
 export default MyPage;
