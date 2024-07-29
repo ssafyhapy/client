1200aa8b78a5851b25abd975c8a63632424dcad2
이명욱 <muk525@icloud.com>
2024-07-29 11:14:09 +0900

    feat:스크롤 바 숨기기(My_page)

--- src/components/MyPageFrame.jsx
+++ src/components/MyPageFrame.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MyPageFrame = ({ children }) => {
   return (
-    <div className="w-[1024px] h-[600px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] rounded-[30px] border-[10px] border-[rgba(255,255,255,0.2)] absolute z-10 flex justify-center items-start p-10 overflow-hidden overflow-y-auto">
+    <div className="w-[1024px] h-[600px] bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] rounded-[30px] border-[10px] border-[rgba(255,255,255,0.2)] absolute z-10 flex justify-center items-start p-10 overflow-y-auto scrollbar-hide">
       {children}
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     backgroundImage: {
       "custom-gradient-game":
         "linear-gradient(135deg, #F1C2D5 15%, #B3E5FC 50%, #D1C4E9 100%)",
-      "custom-gradient-main":
-        "linear-gradient(0deg, #DDCEFF 0%, #F8DFF0 100%)"
+      "custom-gradient-main": "linear-gradient(0deg, #DDCEFF 0%, #F8DFF0 100%)",
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        ".scrollbar-hide": {
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+          "scrollbar-width": "none", // Firefox
+        },
+      };
+      addUtilities(newUtilities);
+    }),
+  ],
 };
