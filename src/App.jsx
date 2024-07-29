4d44979817671034db2994dcfd8a521e59fd626f
heejoo <heejookim99@gmail.com>
2024-07-29 09:11:29 +0900

    feat:기념사진촬영 페이지 (components)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Play from "./pages/Play";
 import MyPage from "./pages/MyPage";
+import Photograph from "./pages/Photograph";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/play" element={<Play />} />
       <Route path="/login" element={<Login />} />
       <Route path="/mypage" element={<MyPage />} />
+      <Route path="/photo" element={<Photograph />} />
     </Routes>
     </>
   );
