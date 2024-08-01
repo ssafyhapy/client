c48393d522558d5c0673dd3f99d74f82f459bc28
이명욱 <muk525@icloud.com>
2024-08-01 14:08:59 +0900

    fix:백엔드 요청 url 수정

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -29,7 +29,7 @@ const Auth = () => {
     const fetchCode = async () => {
       try {
         const response = await axios.post(
-          "https://i11c209.p.ssafy.io:8080/api/oauth/login",
+          "https://i11c209.p.ssafy.io/api/oauth/login",
           {
             registrationId: "kakao",
             authorization: code,
