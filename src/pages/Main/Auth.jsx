010baeb6fc2989664acf6874b1368ecd6452ba28
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-08-02 06:19:42 +0900

    fix:axios 요청

--- src/pages/Main/Auth.jsx
+++ src/pages/Main/Auth.jsx
@@ -13,7 +13,7 @@ const Auth = () => {
   const [error, setError] = useState(null);
 
   // zustand store
-  const {memberName, isLogin, login} = useAuthStore();
+  const { memberName, isLogin, login } = useAuthStore();
 
   // URLSearchParams를 사용하여 인가코드를 가져옴
   useEffect(() => {
@@ -31,8 +31,8 @@ const Auth = () => {
     const fetchCode = async () => {
       setIsLoading(true);
       try {
-        const response = await axiosInstance.post(
-          "/oauth/login",
+        const response = await axios.post(
+          "https://i11c209.p.ssafy.io/api/oauth/login",
           {
             registrationId: "kakao",
             authorization: code,
@@ -40,7 +40,7 @@ const Auth = () => {
           // headers 생략 가능(default 값임), 특정 API 요구 사항이나 커스텀 헤더 필요할 때 헤더 설정
           {
             headers: {
-              ...{},
+              "Content-Type": "application/json",
             },
             // 쿠키를 포함하는 옵션
             withCredentials: true,
@@ -78,7 +78,7 @@ const Auth = () => {
   useEffect(() => {
     console.log("zustand", memberName, isLogin);
   }, [memberName, isLogin]);
-  
+
   return (
     <div>
       {/* 로딩 중일 때 Spinner 컴포넌트를 렌더링 */}
@@ -92,52 +92,69 @@ const Auth = () => {
 
 export default Auth;
 
-  // // 인가코드가 있을 경우 서버로 요청을 보내어 토큰을 받아옴
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   // 서버로 인가코드를 보내어 토큰을 받아오는 함수
-  //   const fetchCode = async () => {
-  //     try {
-  //       const response = await axios.post(
-  //         "https://i11c209.p.ssafy.io/api/oauth/login",
-  //         {
-  //           registrationId: "kakao",
-  //           authorization: code,
-  //         },
-  //         // headers 생략 가능(default 값임), 특정 API 요구 사항이나 커스텀 헤더 필요할 때 헤더 설정
-  //         {
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //           },
-  //           // 쿠키를 포함하는 옵션
-  //           withCredentials: true,
-  //         }
-  //       );
-  //       const result = response.data;
+// // 인가코드가 있을 경우 서버로 요청을 보내어 토큰을 받아옴
+// useEffect(() => {
+//   setIsLoading(true);
+//   // 서버로 인가코드를 보내어 토큰을 받아오는 함수
+//   const fetchCode = async () => {
+//     try {
+//       const response = await axios.post(
+//         "https://i11c209.p.ssafy.io/api/oauth/login",
+//         {
+//           registrationId: "kakao",
+//           authorization: code,
+//         },
+//         // headers 생략 가능(default 값임), 특정 API 요구 사항이나 커스텀 헤더 필요할 때 헤더 설정
+//         {
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           // 쿠키를 포함하는 옵션
+//           withCredentials: true,
+//         }
+//       );
+//       const result = response.data;
 
-  //       // zustand store에 사용자 이름 저장 및 로그인 상태 변경(true)
-  //       console.log("memberName", result.data.memberName);
-  //       login(result.data.memberName);
+//       // zustand store에 사용자 이름 저장 및 로그인 상태 변경(true)
+//       console.log("memberName", result.data.memberName);
+//       login(result.data.memberName);
 
-  //       const headerData = response.headers;
-  //       console.log(
-  //         "accessToken",
-  //         headerData["authorization"].replace(/^Bearer\s/, "")
-  //       );
-  //       const accessToken = headerData["authorization"].replace(
-  //         /^Bearer\s/,
-  //         ""
-  //       );
-  //       // 토큰을 cessionStorage에 저장
-  //       sessionStorage.setItem("accessToken", accessToken);
-  //       // play로 리다이렉트
-  //       navigate("/play");
-  //     } catch (err) {
-  //       setError(err);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
+//       const headerData = response.headers;
+//       console.log(
+//         "accessToken",
+//         headerData["authorization"].replace(/^Bearer\s/, "")
+//       );
+//       const accessToken = headerData["authorization"].replace(
+//         /^Bearer\s/,
+//         ""
+//       );
+//       // 토큰을 cessionStorage에 저장
+//       sessionStorage.setItem("accessToken", accessToken);
+//       // play로 리다이렉트
+//       navigate("/play");
+//     } catch (err) {
+//       setError(err);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
 
-  //   fetchCode();
-  // }, [code, login, navigate]);
+//   fetchCode();
+// }, [code, login, navigate]);
+
+// try {
+//   const response = await axiosInstance.post(
+//     "/oauth/login",
+//     {
+//       registrationId: "kakao",
+//       authorization: code,
+//     },
+//     // headers 생략 가능(default 값임), 특정 API 요구 사항이나 커스텀 헤더 필요할 때 헤더 설정
+//     {
+//       headers: {
+//         ...{},
+//       },
+//       // 쿠키를 포함하는 옵션
+//       withCredentials: true,
+//     }
+//   );
