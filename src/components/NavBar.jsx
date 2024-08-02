6f286598b229133cf35630427b0ad30d96d30a74
이명욱 <muk525@icloud.com>
2024-08-02 09:23:31 +0900

    feat:로그아웃 메세지 출력 및 상태 확인

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Login from "../pages/Main/Login";
 import useAuthStore from "../store/useAuthStore";
@@ -8,7 +8,11 @@ const NavBar = () => {
   const location = useLocation();
   const path = location.pathname;
 
+  // 로그인 모달 상태 추가
   const [loginOpen, setLoginOpen] = useState(false);
+  // 로그아웃 메시지 상태 추가
+  const [logoutMessage, setLogoutMessage] = useState("");
+
   const openLogin = () => {
     setLoginOpen(true);
   };
@@ -16,23 +20,40 @@ const NavBar = () => {
     setLoginOpen(false);
   };
 
-  const { isLogin, logout } = useAuthStore();
+  const { isLogin, logout, memberName } = useAuthStore();
 
   const handleLogout = async () => {
     try {
-      // 로그아웃 AP 호출
-      await axiosInstance.post("/member/logout", null, { withCredentials: true });
+      // 로그아웃 API 호출
+      await axiosInstance.post("/member/logout", null, {
+        withCredentials: true,
+      });
 
       // 세션 스토리지에서 액세스 토큰 삭제
       sessionStorage.removeItem("accessToken");
 
       // 로그아웃 상태 업데이트
       logout();
+
+      // 로그아웃 메시지 설정
+      setLogoutMessage("로그아웃 되었습니다.");
+
+      // 3초 후 로그아웃 메시지 숨김
+      setTimeout(() => {
+        setLogoutMessage("");
+      }, 3000);
+
     } catch (error) {
       console.error("로그아웃 실패", error);
     }
   };
 
+  // zustand store 변경 시 로그인 상태 확인
+  useEffect(() => {
+    console.log("zustand", memberName, isLogin);
+  }, [memberName, isLogin]);
+
+  // navBar의 path에 따라 다른 메뉴 출력(메인)
   if (path === "/") {
     return (
       <nav className="flex flex-col items-center">
@@ -49,6 +70,8 @@ const NavBar = () => {
         </div>
       </nav>
     );
+
+  // navBar의 path에 따라 다른 메뉴 출력(메인 외)
   } else {
     return (
       <>
@@ -62,6 +85,7 @@ const NavBar = () => {
                 <Link to="/play">Play</Link>
               </li>
             </div>
+            {/*  로그인 상태에 따라 메뉴 변경 */}
             <div className="flex gap-8">
               {isLogin ? (
                 <>
@@ -85,6 +109,15 @@ const NavBar = () => {
             <div className="border-b-2 border-solid border-white w-full"></div>
           </div>
         </nav>
+
+        {/* 로그아웃 메시지 출력 */}
+        {logoutMessage && (
+          <div className="absolute top-10 bg-green-200 text-green-800 p-2 rounded">
+            {logoutMessage}
+          </div>
+        )}
+
+        {/* 로그인 모달 */}
         {loginOpen && <Login closeLogin={closeLogin} />}
       </>
     );
