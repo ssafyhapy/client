1688a6878d9e5664ba0f039aee88a2d224db7ad6
이명욱 <muk525@icloud.com>
2024-08-02 14:19:52 +0900

    feat:useAuthStore-persist 전역 상태 유지(Login)

--- src/store/useAuthStore.jsx
+++ src/store/useAuthStore.jsx
@@ -1,10 +1,20 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useAuthStore = create((set) => ({
-  memberName: null,
-  isLogin: null,
-  login: (memberName) => set({ memberName, isLogin: true }),
-  logout: () => set({ memberName: null, isLogin: false }),
-}));
+const useAuthStore = create(
+  // persist 미들웨어를 사용하여 sessionStorage에 상태 저장
+  persist(
+    (set) => ({
+      memberName: null,
+      isLogin: null,
+      login: (memberName) => set({ memberName, isLogin: true }),
+      logout: () => set({ memberName: null, isLogin: false }),
+    }),
+    {
+      name: "auth-storage",
+      getStorage: () => sessionStorage,
+    }
+  )
+);
 
 export default useAuthStore;
