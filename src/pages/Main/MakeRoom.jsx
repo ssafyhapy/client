a6dc79b79f47fb92666053f4e888e2485dca8396
이명욱 <muk525@icloud.com>
2024-08-02 16:27:51 +0900

    feat:방 만들기, 들어가기 백엔드 api 연동

--- src/pages/Main/MakeRoom.jsx
+++ src/pages/Main/MakeRoom.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { axiosInstance } from "../../api/apiClient";
 // import { useNavigate } from "react-router-dom";
 
 const MakeRoom = ({ closeMakeRoom }) => {
@@ -17,12 +18,10 @@ const MakeRoom = ({ closeMakeRoom }) => {
   const onSubmit = async (data) => {
     console.log(data);
     try {
-      const response = await axios.post(
-        "http://i11c209.p.ssafy.io/api/room/create",{
-          roomName: data.roomName,
-          roomPersonCount: data.roomPersonCount
-        }
-      );
+      const response = await axiosInstance.post("/room/create", {
+        roomName: data.roomName,
+        roomPersonCount: data.roomPersonCount,
+      });
       console.log(response);
       // 방 만들기 요청 완료시 대기실로 이동
       // navigate("/waiting_room");
@@ -68,7 +67,9 @@ const MakeRoom = ({ closeMakeRoom }) => {
               {...register("roomPersonCount", { required: true })}
             />
             {errors.roomPersonCount && (
-              <p className="text-red-500 text-sm">{errors.roomPersonCount.message}</p>
+              <p className="text-red-500 text-sm">
+                {errors.roomPersonCount.message}
+              </p>
             )}
           </div>
           <div className="flex justify-end">
@@ -77,9 +78,7 @@ const MakeRoom = ({ closeMakeRoom }) => {
               type="submit"
               className="bg-blue-500 text-white py-2 px-4 rounded"
             >
-              <Link to="/camera-check">
-                방 만들기
-              </Link>
+              <Link to="/camera-check">방 만들기</Link>
             </button>
             {/* 취소 */}
             <button
