6e65dc95bbfefa83829bfd1c494c4c857f33637c
dhkang0912 <dhkang960912@gmail.com>
2024-08-11 01:18:57 +0900

    fix:동영상 정렬 수정 (main)

--- src/components/Photo/PhotographFirst.jsx
+++ src/components/Photo/PhotographFirst.jsx
@@ -56,9 +56,9 @@ const PhotographFirst = () => {
   const getVideoContainerClass = () => {
     const count = 1 + subscribers.length;
     // const count = 6;
-    if (count === 1) return "w-[80%] max-w-[250px] min-w-[100px]";
-    if (count === 2) return "w-[60%] max-w-[200px] min-w-[100px]";
-    if (count >= 3) return "w-[40%] max-w-[150px] min-w-[100px]";
+    if (count === 1) return "w-[80%] max-w-[250px] min-w-[250px]";
+    if (count === 2) return "w-[60%] max-w-[200px] min-w-[200px]";
+    if (count >= 3) return "w-[40%] max-w-[150px] min-w-[150px]";
   };
 
   const getMicIcon = (isAudioActive) => {
