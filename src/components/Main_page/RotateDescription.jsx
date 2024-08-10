9808224ceee8328f227c96a54f1ea04815da99e0
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-08-10 19:44:21 +0900

    comment:props 주석 수정

--- src/components/Main_page/RotateDescription.jsx
+++ src/components/Main_page/RotateDescription.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
-const RotateDescription = ({ sentences, fontsize="64px", time=3000 }) => {
+const RotateDescription = ({ sentences, fontsize="64px", time=2000 }) => {
   const [index, setIndex] = useState(0);
   // const sentences = [
   //   "오프라인에서의 어색한 첫 만남, 걱정되지는 않으신가요?",
@@ -12,7 +12,7 @@ const RotateDescription = ({ sentences, fontsize="64px", time=3000 }) => {
   useEffect(() => {
     const timer = setInterval(() => {
       setIndex((prevIndex) => (prevIndex + 1) % sentences.length);
-    }, time); // 5초마다 문장 변경
+    }, time); // (time)초마다 문장 변경
     return () => clearInterval(timer);
   }, []);
 
@@ -26,7 +26,7 @@ const RotateDescription = ({ sentences, fontsize="64px", time=3000 }) => {
           exit={{ y: -20, opacity: 0 }}
           transition={{ duration: 0.5 }}
           style={{fontSize: fontsize}}
-          className="font-bold text-center text-[rgba(0,0,0,0.5)]"
+          className="font-bold text-center text-[rgba(0,0,0,0.5)] whitespace-nowrap"
         >
           {sentences[index]}
         </motion.div>
