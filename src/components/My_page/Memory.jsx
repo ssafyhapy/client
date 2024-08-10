a076b6cc0dfe864fe2fc7194bfce8edc23564c23
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-08-11 00:00:22 +0900

    fix:추억상자 화면 렌더링

--- src/components/My_page/Memory.jsx
+++ src/components/My_page/Memory.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 const Memory = ({ memorybox }) => {
-  return <div>{memorybox}</div>;
+  return (
+    <div className="flex flex-col">
+      <img src={memorybox.memberMemoryImageUrl} alt={memorybox.memberMemoryBoxName || "추억 상자 이미지"} />
+      <p>{memorybox.memberMemoryBoxName}</p>
+    </div>
+  );
 };
 export default Memory;
