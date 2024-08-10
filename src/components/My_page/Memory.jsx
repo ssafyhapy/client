45a142b76e25506a1e08db5ba29b87c18dfbed73
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-08-11 01:53:50 +0900

    design:추억 상자(My_page)

--- src/components/My_page/Memory.jsx
+++ src/components/My_page/Memory.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import defaultImage from "../../assets/My_page/defaultImage.svg";
+import defaultImage from "../../assets/My_page/defaultImg.jpg";
 
 const Memory = ({ memorybox }) => {
   return (
-    <div className="w-[240px] h-[350px] bg-white flex flex-col justify-between items-center">
-      <img
-        className="w-[200px] h-[300px]"
-        src={memorybox.memberMemoryImageUrl || { defaultImage }}
-        alt={memorybox.memberMemoryBoxName || "방 제목 "}
-      />
-      <div>
-        <p className="text-left">{memorybox.memberMemoryBoxDate}</p>
-        <p className="text-right">{memorybox.memberMemoryBoxName}</p>
+    <div className="flex">
+      <div className="w-[240px] h-[350px] bg-white flex flex-col justify-between items-center p-4">
+        <img
+          className="w-[200px] h-[300px]"
+          src={memorybox.memberMemoryImageUrl || defaultImage}
+          alt={memorybox.memberMemoryBoxName || "방 제목"}
+        />
+        <div className="w-full">
+          <p className="text-left">{memorybox.memberMemoryBoxDate}</p>
+          <p className="text-right">{memorybox.memberMemoryBoxName}</p>
+        </div>
       </div>
     </div>
   );
