b4640bf0251b836db08b044c49ac812054a7cc3e
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-08-11 02:02:54 +0900

    design:flexbox 추억상자 배열(My_page)

--- src/components/My_page/MemoryBox.jsx
+++ src/components/My_page/MemoryBox.jsx
@@ -6,13 +6,13 @@ const MemoryBox = () => {
   const { memberMemoryboxList } = useMypageStore();
   return (
     <div className="w-full h-auto bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] flex flex-col p-5 gap-5 relative">
-      <div className="flex items-center gap-2">
-        <h1 className="text-2xl">추억 상자</h1>
-      </div>
+      <h1 className="text-2xl">추억 상자</h1>
       {/* // Memory 컴포넌트를 사용하여 memberMemoryboxList를 순회하며 출력 */}
-      {memberMemoryboxList?.map((memorybox) => (
-        <Memory key={memorybox.roomId} memorybox={memorybox} />
-      ))}
+      <div className="flex gap-4 overflow-x-auto flex-nowrap">
+        {memberMemoryboxList?.map((memorybox) => (
+          <Memory key={memorybox.roomId} memorybox={memorybox} />
+        ))}
+      </div>
     </div>
   );
 };
