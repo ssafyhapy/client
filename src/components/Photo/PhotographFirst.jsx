20b753fb2248ff3b7a593a4ff6d3913f285ebdae
dhkang0912 <dhkang960912@gmail.com>
2024-08-11 16:04:21 +0900

    fix:3인이상 포토 이미지 크기 수정  (main)

--- src/components/Photo/PhotographFirst.jsx
+++ src/components/Photo/PhotographFirst.jsx
@@ -39,7 +39,7 @@ const PhotographFirst = () => {
         // 사진찍고 2초뒤 자동으로 나를 맞춰봐 겟레디 페이지로 이동
         setTimeout(() => {
           // 테스트를 위해 넘어가지 못하게 막기
-          setGameStep("guess-me");
+          // setGameStep("guess-me");
           // navigate("/guessme-getready");
         }, 2000);
       });
