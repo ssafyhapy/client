46a72fabb6e9a0bee5947f92558e3d7f8fabdf3e
이명욱 <muk525@icloud.com>
2024-08-12 14:29:42 +0900

    design:step2, 3 패딩 조정

--- src/components/Report/Step2.jsx
+++ src/components/Report/Step2.jsx
@@ -5,7 +5,7 @@ const Step2 = ({ Ox }) => {
   console.log("Ox", Ox);
 
   return (
-    <div className="w-full h-full bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] rounded-[20px] flex p-5 gap-5 relative">
+    <div className="w-full h-full bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] rounded-[20px] flex py-3-5 gap-5 relative">
       <div className="absolute top-[-10px] left-[-10px] rounded-[20px] bg-white w-[20%] h-[20%] flex justify-center items-center">
         <h1>step 2</h1>
       </div>
--- src/components/Report/Step3.jsx
+++ src/components/Report/Step3.jsx
@@ -5,7 +5,7 @@ const Step3 = ({ Balance }) => {
   console.log("Balance", Balance);
 
   return (
-    <div className="w-full h-full bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] rounded-[20px] flex p-5 gap-5 relative">
+    <div className="w-full h-full bg-[rgba(255,255,255,0.3)] shadow-[0_0_30px_rgba(66,72,81,0.3)] border-[10px] border-[rgba(255,255,255,0.2)] rounded-[20px] flex py-5 gap-5 relative">
       <div className="absolute top-[-10px] left-[-10px] rounded-[20px] bg-white w-[10%] h-[20%] flex justify-center items-center">
         <h1>step 3</h1>
       </div>
