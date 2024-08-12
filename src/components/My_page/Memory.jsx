6f5c0bb3e501266d91ab752ddc7c2032b161e271
이명욱 <muk525@icloud.com>
2024-08-12 13:58:57 +0900

    fix:링크 페이지네이션 충돌

--- src/components/My_page/Memory.jsx
+++ src/components/My_page/Memory.jsx
@@ -4,19 +4,17 @@ import { Link } from "react-router-dom";
 
 const Memory = ({ memorybox, roomId }) => {
   return (
-    <div className="w-[240px] h-[350px] bg-white flex flex-col justify-between items-center p-4 gap-2">
-      <Link to={`/room/${roomId}/report`}>
-        <img
-          className="w-[200px] h-[300px]"
-          src={memorybox.memberMemoryImageUrl || defaultImage}
-          alt={memorybox.memberMemoryBoxName || "방 제목"}
-        />
-        <div className="w-full">
-          <p className="text-left">{memorybox.memberMemoryBoxDate}</p>
-          <p className="text-right">{memorybox.memberMemoryBoxName}</p>
-        </div>
-      </Link>
-    </div>
+    <Link to={`/room/${roomId}/report`} className="w-[240px] h-[350px] bg-white flex flex-col justify-between items-center p-4 gap-2">
+      <img
+        className="w-[200px] h-[300px]"
+        src={memorybox.memberMemoryImageUrl || defaultImage}
+        alt={memorybox.memberMemoryBoxName || "방 제목"}
+      />
+      <div className="w-full">
+        <p className="text-left">{memorybox.memberMemoryBoxDate}</p>
+        <p className="text-right">{memorybox.memberMemoryBoxName}</p>
+      </div>
+    </Link>
   );
 };
 export default Memory;
