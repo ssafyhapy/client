21b71db6a54df2829e2563b3cd702b06a9da19ff
dhkang0912 <dhkang960912@gmail.com>
2024-08-14 22:05:57 +0900

    feat:레이아웃 수정 중  (main)

--- src/components/Common/TopDiv.jsx
+++ src/components/Common/TopDiv.jsx
@@ -35,16 +35,18 @@ const TopDiv = ({ session }) => {
       {gameStep === "waiting-room" ? (
         <div className="flex flex-col items-center justify-center ms-3 ">
           <div className="flex">
-            <div>접속 코드 : {roomCode} </div>
-            <button onClick={handleClipBoard} className="w-[30%] h-[30%]">
-              <img
-                className="w-[15%] h-[15%]"
-                src="https://sarrr.s3.ap-northeast-2.amazonaws.com/assets/clipboard.webp"
-                alt=""
-              />
-            </button>
+            <div>
+              접속 코드 : {roomCode} ≈
+              <button onClick={handleClipBoard} className="w-[30%] h-[30%]">
+                <img
+                  className="w-[15%] h-[15%]"
+                  src="https://sarrr.s3.ap-northeast-2.amazonaws.com/assets/clipboard.webp"
+                  alt=""
+                />
+              </button>
+            </div>
+            <div> 방 이름 : {roomName}</div>
           </div>
-          <div> 방 이름 : {roomName}</div>
           {/* <div> 방 설정 인원 : {roomPersonCount}</div> */}
         </div>
       ) : null}
