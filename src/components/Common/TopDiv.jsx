6c18e6742647296bb0b0eb4c0da406a2c4fbaf0e
dhkang0912 <dhkang960912@gmail.com>
2024-08-15 00:36:18 +0900

    fix:top div layout 수정 중  (main)

--- src/components/Common/TopDiv.jsx
+++ src/components/Common/TopDiv.jsx
@@ -30,12 +30,13 @@ const TopDiv = ({ session }) => {
     setShowModal(false);
   };
 
-  const flexLayout = gameStep==="waiting-room"?"justify-between":"justify-center"
+  const flexLayout =
+    gameStep === "waiting-room" ? "justify-between" : "justify-center";
 
   return (
     <>
-    <div className={`flex items-center w-[90%] p-2 ${flexLayout} relative`}>
-      {gameStep === "waiting-room" ? (
+      <div className={`flex items-center w-[90%] p-2 ${flexLayout} relative`}>
+        {/* {gameStep === "waiting-room" ? (
         <div className="flex flex-col justify-center items-start">
           <div className="flex">
             <div>접속 코드 : {roomCode} </div>
@@ -50,22 +51,36 @@ const TopDiv = ({ session }) => {
           <div className="text-left"> 방 이름 : {roomName}</div>
           <div className="text-left"> 방 설정 인원 : {roomPersonCount}</div>
         </div>
-      ) : null}
-
-      <div>
-        {gameStep == "camera-check" ||
-        gameStep == "waiting-room" ||
-        gameStep == "photo-first" ||
-        gameStep == "photo-last" ? null : (
-          <GameTurns gameStep={gameStep}></GameTurns>
-        )}
+      ) : null} */}
+        {gameStep === "waiting-room" ? (
+          <div className="flex flex-col justify-center items-start">
+            <div className="flex">
+              <div>{roomName}({roomCode}) 
+              <button onClick={handleClipBoard} className="w-[30%] h-[30%]">
+                <img
+                  className="w-[15%] h-[15%]"
+                  src="https://sarrr.s3.ap-northeast-2.amazonaws.com/assets/clipboard.webp"
+                  alt=""
+                />
+              </button>
+              </div>
+            </div>
+            <div className="text-left"> 방 설정 인원 : {roomPersonCount}</div>
+          </div>
+        ) : null}
+        ƒ
+        <div>
+          {gameStep == "camera-check" ||
+          gameStep == "waiting-room" ||
+          gameStep == "photo-first" ||
+          gameStep == "photo-last" ? null : (
+            <GameTurns gameStep={gameStep}></GameTurns>
+          )}
+        </div>
+        {gameStep !== "photo-first" && gameStep !== "photo-last" ? (
+          <ExitBtn session={session} />
+        ) : null}
       </div>
-
-      {gameStep !== "photo-first" && gameStep !== "photo-last" ? (
-        <ExitBtn session={session} />
-      ) : null}
-
-    </div>
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-[rgba(255,255,255,0.95)] p-6 rounded-[30px] shadow-lg flex flex-col justify-center items-center gap-5">
@@ -78,7 +93,7 @@ const TopDiv = ({ session }) => {
           </div>
         </div>
       )}
-      </>
+    </>
   );
 };
 
