cc1e58acec131f635decbdc1535fa427a76bb0be
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-08-15 11:28:45 +0900

    design:서비스 소개 호버 기능 추가 및 whole 슬라이더 삭제

--- src/components/Main_page/service/WholeService.jsx
+++ src/components/Main_page/service/WholeService.jsx
@@ -1,30 +1,12 @@
 import React from "react";
 import NavBar from "../../NavBar";
 import PlayBtn from "../../Buttons/PlayBtn";
-import MainFrame from "../../MainFrame";
-import ScrollDownBtn from "../ScrollDownBtn";
-import RotateDescription from "../RotateDescription";
+import { motion } from "framer-motion";
 
 const WholeService = () => {
-  const [currentIndex, setCurrentIndex] = React.useState(0);
-
-  const nextSlide = () => {
-    setCurrentIndex((currentIndex + 1) % slides.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((currentIndex - 1 + slides.length) % slides.length);
-  };
-
-  const goToSlide = (index) => {
-    setCurrentIndex(index);
-  };
-
   const slides = [
-    "어떤 목적으로 모인 그룹이든",
-    "첫 만남이 어색하지 않도록",
-    "다양한 게임으로",
-    "서로 알아가는 시간을 가지는",
+    "어떤 목적으로 모인 그룹이든 첫 만남이 어색하지 않도록",
+    "다양한 게임으로 서로 알아가는 시간을 가지는",
     "온라인 아이스 브레이킹 서비스",
     "사르르의 게임들을",
     "하나 하나 따라가는 것만으로도",
@@ -34,36 +16,21 @@ const WholeService = () => {
   return (
     <div className="flex flex-col w-[80%] h-[80%] justify-between items-center">
       <NavBar />
-      <div className=" w-full h-full flex flex-col justify-between items-center gap-20">
+      <div className="w-full h-full flex flex-col justify-between items-center gap-10">
         <div />
-        <p className="opacity-50 text-[84px] font-semibold whitespace-nowrap">
-          {slides[currentIndex]}
-        </p>
-
-        <div className="flex gap-10 items-center">
-          <button
-            onClick={prevSlide}
-            className="px-2 py-1 bg-[#C2ACF4] rounded text-white"
-          >
-            prev
-          </button>
-          <div className="flex space-x-2">
-            {slides.map((_, index) => (
-              <span
+        <div className="flex">
+          <div className="w-full flex flex-col items-start gap-4 opacity-50">
+            {slides.map((slide, index) => (
+              <motion.p
                 key={index}
-                onClick={() => goToSlide(index)}
-                className={`block w-2 h-2 rounded-full cursor-pointer ${
-                  index === currentIndex ? "bg-[#9400d3b0]" : "bg-[#9400d354]"
-                }`}
-              />
+                className="text-[30px] font-semibold whitespace-nowrap"
+                whileHover={{ scale: 1.1, color: "#9400d3b0", opacity: 1 }}
+              >
+                {slide}
+              </motion.p>
             ))}
           </div>
-          <button
-            onClick={nextSlide}
-            className="px-2 py-1 bg-[#C2ACF4] rounded text-white"
-          >
-            next
-          </button>
+          <iframe src="https://lottie.host/embed/e61d9aab-2ad6-4958-9b92-59f308e61607/fzeD0O9H5M.json"></iframe>
         </div>
       </div>
       <div className="flex justify-between w-full">
