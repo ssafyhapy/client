98a4d3e7770730ccb26c48f203bea34873e3e700
dhkang0912 <dhkang960912@gmail.com>
2024-08-15 13:08:38 +0900

    fix:top div layout 변경 및 middle div, bottom div 비율 맞춤  (main)

--- src/components/Common/GameBackground.jsx
+++ src/components/Common/GameBackground.jsx
@@ -6,7 +6,7 @@ const GameBackground = ({ children, session }) => {
   const gameStep = useGameStore((state) => state.gameStep);
   return (
     <div className="bg-custom-gradient-game w-full flex justify-around items-center min-h-[100vh]">
-      <div className="w-[90vw] h-[98vh] bg-[rgba(255,255,255,0.3)] rounded-[40px] flex flex-col relative items-center justify-center ">
+      <div className="w-[90vw] h-[98vh] bg-[rgba(255,255,255,0.3)] rounded-[40px] flex flex-col gap-5 relative items-center justify-center ">
         {children}
       </div>
     </div>
--- src/components/Common/TopDiv.jsx
+++ src/components/Common/TopDiv.jsx
@@ -35,7 +35,7 @@ const TopDiv = ({ session }) => {
 
   return (
     <>
-      <div className={`flex items-center w-[90%] p-2 ${flexLayout} relative`}>
+      <div className={`flex items-center w-[95%] p-2 ${flexLayout} relative`}>
         {/* {gameStep === "waiting-room" ? (
         <div className="flex flex-col justify-center items-start">
           <div className="flex">
