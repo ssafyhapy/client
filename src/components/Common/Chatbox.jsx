73dcae6da3f37b206b6247709bdd1146f8a2c39d
heejoo <heejookim99@gmail.com>
2024-08-15 12:41:12 +0900

    fix : chatbox 시스템메시지 줄바꿈 없앰 (main)

--- src/components/Common/Chatbox.jsx
+++ src/components/Common/Chatbox.jsx
@@ -194,9 +194,6 @@ const Chatbox = () => {
                   : "bg-[rgba(14,107,255,0.5)] text-white text-sm"
                 }`}
                 style={{ maxWidth: "75%" }}
-                dangerouslySetInnerHTML={{
-                  __html: msg.message.replace(/\n/g, "<br />"),
-                }}
               >
                 {msg.message}
               </div>
